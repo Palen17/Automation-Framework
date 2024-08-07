d2cfd3114dff1d4bc5a26e78aa9e48eed2848d30
valen napolitano <valentinnapo@gmail.com>
2024-08-07 13:40:42 -0300

    Selenium Grid/Docker

--- src/test/java/testBase/BaseClass.java
+++ src/test/java/testBase/BaseClass.java
@@ -4,11 +4,14 @@ import org.apache.commons.lang3.RandomStringUtils;
 import org.apache.logging.log4j.LogManager; //Log4j
 import org.apache.logging.log4j.Logger; //Log4j
 import org.openqa.selenium.OutputType;
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Parameters;
@@ -17,6 +20,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.time.Duration;
 import java.util.Date;
@@ -39,19 +43,62 @@ public class BaseClass {
 
         logger = LogManager.getLogger(this.getClass());
 
-        switch (br.toLowerCase()){
-            case "chrome": driver = new ChromeDriver();
-                break;
+        if (p.getProperty("execution_env").equalsIgnoreCase("remote")){
+            DesiredCapabilities capabilities = new DesiredCapabilities();
 
-            case "edge": driver = new EdgeDriver();
-                break;
-
-            case "firefox": driver = new FirefoxDriver();
-                break;
-
-            default:
-                System.out.println("Invalid browser name..");
+            //os
+            if (os.equalsIgnoreCase("windows")){
+                capabilities.setPlatform(Platform.WIN10);
+            }
+            else if (os.equalsIgnoreCase("mac")){
+                capabilities.setPlatform(Platform.MAC);
+            }
+            else if (os.equalsIgnoreCase("linux")){
+                capabilities.setPlatform(Platform.LINUX);
+            }
+            else {
+                System.out.println("Not matching os");
                 return;
+            }
+
+            //browser
+            switch (br.toLowerCase()){
+                case "chrome": capabilities.setBrowserName("chrome");
+                    break;
+
+                case "edge": capabilities.setBrowserName("MicrosoftEdge");
+                    break;
+
+                case "firefox": capabilities.setBrowserName("firefox");
+                    break;
+
+                default:
+                    System.out.println("Not matching browser");
+                    return;
+
+            }
+
+            driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), capabilities);
+
+        }
+
+        if (p.getProperty("execution_env").equalsIgnoreCase("local")){
+
+            switch (br.toLowerCase()){
+                case "chrome": driver = new ChromeDriver();
+                    break;
+
+                case "edge": driver = new EdgeDriver();
+                    break;
+
+                case "firefox": driver = new FirefoxDriver();
+                    break;
+
+                default:
+                    System.out.println("Invalid browser name..");
+                    return;
+
+            }
 
         }
 
@@ -59,6 +106,7 @@ public class BaseClass {
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.get(p.getProperty("appURL"));
         driver.manage().window().maximize();
+
     }
 
     @AfterClass(groups = {"Sanity", "Regression", "Master"})
